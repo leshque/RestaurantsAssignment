08e1b4af503fb87e04e39e59e80e48fd7cd3c286
Aliaksei Prokharau <am.prokhorov@gmail.com>
2022-05-21 14:54:58 +0200

    Initial cleanup

---
+++ RestorauntsApp/Shared/AppEnvironment.swift
@@ -0,0 +1,14 @@
+//
+//  AppEnvironment.swift
+//  RestorauntsApp
+//
+//  Created by Aliaksei Prokharau on 21.05.22.
+//
+
+import Foundation
+
+class AppEnvironment {
+    
+    static let shared: AppEnvironment = AppEnvironment()
+    
+}

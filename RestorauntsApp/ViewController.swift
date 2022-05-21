fac31d9e8035dbe67f6e2d243b96116e05e64f7e
Aliaksei Prokharau <am.prokhorov@gmail.com>
2022-05-21 14:41:14 +0200

    Initial commit

---
+++ RestorauntsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestorauntsApp
+//
+//  Created by Aliaksei Prokharau on 21.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
